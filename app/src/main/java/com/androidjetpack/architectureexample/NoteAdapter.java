b9eaac33803318362d533c7f89a40feb80d2abac
Ganpat <ganpatmalekar@gmail.com>
2020-05-22 16:12:41 +0530

    Initial commit

---
+++ app/src/main/java/com/androidjetpack/architectureexample/NoteAdapter.java
@@ -0,0 +1,87 @@
+package com.androidjetpack.architectureexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends ListAdapter<Note, NoteAdapter.NoteHolder> {
+    private OnItemClickListener listener;
+
+    public NoteAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle()) &&
+                    oldItem.getDescription().equals(newItem.getDescription()) &&
+                    oldItem.getPriority() == newItem.getPriority();
+        }
+    };
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = getItem(position);
+        holder.textView_title.setText(currentNote.getTitle());
+        holder.textView_description.setText(currentNote.getDescription());
+        holder.textView_priority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    public Note getNoteAt(int position) {
+        return getItem(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView_title;
+        private TextView textView_description;
+        private TextView textView_priority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textView_title = itemView.findViewById(R.id.text_view_title);
+            textView_description = itemView.findViewById(R.id.text_view_description);
+            textView_priority = itemView.findViewById(R.id.text_view_priority);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if (listener != null && pos != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(getItem(pos));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Note note);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
